147deceeb128eca85111a1e9a3cf679536ae515a
alphacentaura <Alphacentaura@bk.ru>
2019-09-09 19:10:37 +0300

    1

--- UIElements2/ViewController.swift
+++ UIElements2/ViewController.swift
@@ -10,11 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var stepper: UIStepper! {
+        didSet {
+            stepper.value = 17
+            stepper.minimumValue = 10
+            stepper.maximumValue = 25
+            stepper.tintColor = .white
+            stepper.backgroundColor = .gray
+            stepper.layer.cornerRadius = 5
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.layer.cornerRadius = 10
+        textView.backgroundColor = view.backgroundColor
+        textView.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height-bottomConstraint.constant, right: 0)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        textView.contentInset = UIEdgeInsets.zero
+    }
+    
+    @IBAction func sizeFontChange(_ sender: UIStepper) {
+        let font = textView.font?.fontName
+        let fontSize = CGFloat(sender.value)
+        textView.font = UIFont(name: font!, size: fontSize)
+    }
+    
 }
 
+extension ViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = .white
+        textView.textColor = .gray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = self.view.backgroundColor
+        textView.textColor = .black
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        label.text = "\(textView.text.count)"
+        return textView.text.count + (text.count - range.length) <= 60
+    }
+}
