68899d96ada447c7fe876e73c0a50f757506122b
alphacentaura <Alphacentaura@bk.ru>
2019-09-10 14:51:38 +0300

    - added activity indicator - added progressBar

--- UIElements2/ViewController.swift
+++ UIElements2/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
             stepper.layer.cornerRadius = 5
         }
     }
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +32,39 @@ class ViewController: UIViewController {
         textView.backgroundColor = view.backgroundColor
         textView.delegate = self
         
+        textView.isHidden = true
+        //textView.alpha = 0
+        
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
+        // Activiti indicator
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+//        UIView.animate(withDuration: 1, delay: 2, options: .curveEaseInOut, animations: {
+//            self.textView.alpha = 1
+//        }) { (finished) in
+//            self.activityIndicator.startAnimating()
+//            self.textView.isHidden = false
+//            UIApplication.shared.endIgnoringInteractionEvents()
+//        }
+        
+        // Progress
+        progressView.setProgress(0, animated: true)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if self.progressView.progress != 1 {
+                self.progressView.progress += 0.2
+            } else {
+                self.activityIndicator.startAnimating()
+                self.textView.isHidden = false
+                UIApplication.shared.endIgnoringInteractionEvents()
+                self.progressView.isHidden = true
+            }
+        }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
